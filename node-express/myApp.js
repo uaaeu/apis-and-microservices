d95b9e9396b6cd80fe7a331999f2ba994f594732
uaaeu <yurtmusa@gmail.com>
2020-07-15 18:27:54 +0300

    serve JSON on a specific route

--- node-express/myApp.js
+++ node-express/myApp.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => res.sendFile(`${__dirname}/views/index.html`))
 app.use(express.static(`${__dirname}/public`))
 
 /** 5) serve JSON on a specific route */
-
+app.get("/json", (req, res) => res.json({"message": "Hello json"}))
 
 /** 6) Use the .env file to configure the app */
  
