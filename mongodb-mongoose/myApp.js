1f1e6184375f1ae01fa6b64187a789a55484a42f
uaaeu <yurtmusa@gmail.com>
2020-07-16 18:54:10 +0300

    Create a 'Person' Model

--- mongodb-mongoose/myApp.js
+++ mongodb-mongoose/myApp.js
@@ -42,8 +42,14 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // `default` values. See the [mongoose docs](http://mongoosejs.com/docs/guide.html).
 
 // <Your code here >
+const peopleSchema = new mongoose.Schema({
+  name : {type: String, required: true },
+  age : Number,
+  favoriteFoods : [String]
+});
 
-var Person /* = <Your Model> */
+const Person = mongoose.model('Person', peopleSchema);
+ /* = <Your Model> */
 
 // **Note**: Glitch is a real server, and in real servers interactions with
 // the db are placed in handler functions, to be called when some event happens
